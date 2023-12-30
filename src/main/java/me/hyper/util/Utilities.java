1c58bfde6db8d8baec80f3e27a98c0d9cffa033f
hyper <honkhonk4456@gmail.com>
2023-12-29 23:29:14 -0500

    Change case for namespace & yellow potato crop update

--- src/main/java/me/hyper/util/Utilities.java
+++ src/main/java/me/hyper/util/Utilities.java
@@ -1,7 +1,10 @@
 package me.hyper.util;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.impl.content.registry.CompostingChanceRegistryImpl;
 import net.minecraft.block.Block;
+import net.minecraft.block.ComposterBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
@@ -13,16 +16,17 @@ import net.minecraft.util.Identifier;
 public class Utilities {
 
     public static void registerItem(String path, Item item) {
-        Registry.register(Registries.ITEM, new Identifier("potatoesplus", path), item);
+        Registry.register(Registries.ITEM, new Identifier("PotatoesPlus", path), item);
+        CompostingChanceRegistry.INSTANCE.add(item, 65f);
     }
 
     public static void registerBlock(String path, Block block) {
-        Registry.register(Registries.BLOCK, new Identifier("potatoesplus", path), block);
+        Registry.register(Registries.BLOCK, new Identifier("PotatoesPlus", path), block);
     }
 
     public static void registerCrop(String path, Block block, Item item) {
-        Registry.register(Registries.ITEM, new Identifier("potatoesplus", path), item);
-        Registry.register(Registries.BLOCK, new Identifier("potatoesplus", path + "_crop_block"), block);
+        Registry.register(Registries.ITEM, new Identifier("PotatoesPlus", path), item);
+        Registry.register(Registries.BLOCK, new Identifier("PotatoesPlus", path + "_crop_block"), block);
     }
 
     public static void addItemAfter(RegistryKey<ItemGroup> group, ItemConvertible firstItem, ItemConvertible secondItem) {
